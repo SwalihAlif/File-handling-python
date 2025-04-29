aa6c72e54cddfb68c6af75c7d1ca0a8292a736ae
Muhammed Swalih M <zzray485@gmail.com>
2025-04-29 17:41:44 +0530

    practiced some example notes

--- notes_manager/main.py
+++ notes_manager/main.py
@@ -1,13 +1,15 @@
+# to run this in termianl command => python main.py
+
 import os
 
 NOTES_DIR = "notes"
 
-# ensure the notes folder exists
+# ensure the notes folder exists, if not it will create a new one.
 if not os.path.exists(NOTES_DIR):
     os.makedirs(NOTES_DIR)
 
 def create_note():
-    title = input("Enter not title: ").strip()
+    title = input("Enter note title: ").strip()
     content = input("Enter note content:\n")
     with open(f"{NOTES_DIR}/{title}.txt", "w") as f:
         f.write(content)
